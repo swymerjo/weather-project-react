4f4403003a90ed9a4e1d4ee12e8cf78b8376c35d
swymerjo <80417034+swymerjo@users.noreply.github.com>
2021-11-21 13:15:22 +0100

    added open source link

--- src/Source.js
+++ src/Source.js
@@ -6,7 +6,7 @@ export default function Source(){
     return(
         <div className="source">
             <p>
-                Open source code by
+                <a className="open-source-link" href="https://github.com/swymerjo/weather-project-react">Open source code </a> by
                 <a className="name-link" href ="https://jovial-roentgen-8bfbb8.netlify.app"> Sammy Wymer </a> 
                 from <a className="website-link" href="https://app.netlify.com/sites/jovial-roentgen-8bfbb8/overview">undersTand'em </a> </p>
         </div>
